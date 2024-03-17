5ab213f2c807f1090475781fc11cc22ce5c45ff6
sssplCodeStar <ssspl.official@gmail.com>
2024-03-17 23:47:45 +0530

    nfc attendance route added

--- models/SalaryRequest.js
+++ models/SalaryRequest.js
@@ -1,12 +1,15 @@
 const { default: mongoose } = require("mongoose");
 
 const salaryRequestSchema = new mongoose.Schema({
-    applyDate: { type: Date },
-    amount: { type: Number },
-    userId: { type: mongoose.Types.ObjectId, ref: "users" },
-    facultyId: { type: mongoose.Types.ObjectId, ref: "faculties" },
-    forwardToHead: { isForwarded: { type: Boolean }, status: { type: String }, date: { type: Date } },
-    forwardToAdminDept: { isForwarded: { type: Boolean }, status: { type: String } },
+    applyDate: { type: Date, required: true },
+    amount: { type: Number, required: true },
+    dateFrom: { type: Date, required: true },
+    dateTo: { type: Date, required: true },
+    contactNo: { type: Number, required: true },
+    userId: { type: mongoose.Types.ObjectId, ref: "users", required: true },
+    facultyId: { type: mongoose.Types.ObjectId, ref: "faculties", required: true },
+    forwardToHead: { isForwarded: { type: Boolean, default: false }, status: { type: String, default: "pending" }, date: { type: Date } },
+    forwardToAdminDept: { isForwarded: { type: Boolean, default: false }, status: { type: String, default: "pending" } },
 }, { timestamps: true });
 
 module.exports = mongoose.model("SalaryRequests", salaryRequestSchema);
