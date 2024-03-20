7d326e4dcab2dcf96817247001f2e69f72a1c132
sssplCodeStar <ssspl.official@gmail.com>
2024-03-21 01:07:17 +0530

    changes for header stats

--- routes/faculties.js
+++ routes/faculties.js
@@ -1,15 +1,17 @@
 const { getFaculties } = require('../controllers');
 const { createFaculty, getFaculty, modifyFaculty, deleteFaculty, getSingleDaySchedule } = require('../controllers');
+const { getFacultyHeaderStatus } = require('../controllers/faculty.controller');
 const { authM } = require('../middleware');
 
 const router = require('express').Router();
 
 router
     .get("/faculty/:fId", getFaculty)
+    .get("/faculty/v1/headerstats", authM, getFacultyHeaderStatus) // /faculty/v1/headerstats?month=
     .get("/getFaculties", authM, getFaculties)
     .post("/faculty", authM, createFaculty)
     .put("/faculty/:fId", modifyFaculty)
     .delete("/faculty/:fId", deleteFaculty)
     .get("/faculty/schedule/:day", authM, getSingleDaySchedule);
-    
+
 module.exports = router;
--- services/attendance.service.js
+++ services/attendance.service.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require("mongoose");
 const { Attendance } = require("../models")
 module.exports = {
     async getTodaysAttendance(fId, targetDate) {
@@ -6,8 +7,44 @@ module.exports = {
 
         var endOfDay = new Date(targetDate);
         endOfDay.setUTCHours(23, 59, 59, 999);
-        
+
         const result = Attendance.findOne({ facultyId: fId, date: { $gte: startOfDay.toISOString(), $lte: endOfDay.toISOString() } });
         return result;
+    },
+    async aggregateSalary(roleId, month) {
+        const result = await Attendance.aggregate([
+            {
+                $match: {
+                    facultyId: new mongoose.Types.ObjectId(roleId),
+                    $expr: {
+                        "$eq": [{ "$month": "$date" }, parseInt(month)]
+                    }
+                }
+            },
+            {
+                $project: {
+                    // countTH: { $sum: { $cond: { "if": { "$eq": ["$attendance.teachingType", "TH"] }, then: 1, else: 0 } } },
+                    // countPR: { $sum: { $cond: { "if": { "$eq": ["$attendance.teachingType", "PR"] }, then: 1, else: 0 } } },
+                    // count: { $size: "$attendance" },
+                    // totalAmount: { $sum: "$attendance.amount" },
+                    attendance: "$attendance"
+                },
+            },
+            {
+                $unwind: "$attendance"
+            },
+            {
+                $group: {
+                    _id: 0,
+                    totalTH: { $sum: { $cond: { "if": { "$eq": ["$attendance.teachingType", "TH"] }, then: 1, else: 0 } } },
+                    totalPR: { $sum: { $cond: { "if": { "$eq": ["$attendance.teachingType", "PR"] }, then: 1, else: 0 } } },
+                    totalAttendence: { $sum: 1 },
+                    totalSalary: { $sum: "$attendance.amount" }
+                }
+            }
+
+        ]);
+        // console.log(result);
+        return result[0] || { _id: 0, totalAttendence: 0, totalSalary: 0 };
     }
 }
